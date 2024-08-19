2697a5db1fa675e395bb6a7011dc5714a1d0c95a
mohanRaj <mohanraj@mothercode.com>
2024-08-19 12:51:33 +0530

    Update build.gradle, CashCardJsonTest and CashCardApplicationTests add test block for better test output add test shouldReturnACashCardWhenDataIsSaved to CashCardApplicationTests add test cashCardDeSerializationTest to CashCardJsonTest

--- cashcard/src/test/java/example/cashcard/CashCardJsonTest.java
+++ cashcard/src/test/java/example/cashcard/CashCardJsonTest.java
@@ -1,18 +1,31 @@
 package example.cashcard;
 
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
-
-import java.io.IOException;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
 class CashCardJsonTest {
 
     @Autowired
     private JacksonTester<CashCard> json;
+
+    @Test
+    void cashCardDeSerializationTest() throws IOException {
+        String expected = """
+         {
+            "id": 99,
+            "amount": 123.45
+         }
+         """;
+        assertThat(json.parse(expected)).isEqualTo(new CashCard(99L, 123.45));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99L);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+    }
 
     @Test
     void cashCardSerializationTest() throws IOException {
