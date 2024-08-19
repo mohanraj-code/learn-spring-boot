5f12029e60b163f0d50d1e7ab788819f53ea750d
mohanRaj <mohanraj@mothercode.com>
2024-08-19 16:01:36 +0530

    Update CashcardApplicationTest.java add test shouldNotReturnACashCardWithAnUnknownId update test shouldReturnACashCardWhenDataIsSaved

--- cashcard/src/test/java/example/cashcard/CashcardApplicationTests.java
+++ cashcard/src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -1,7 +1,6 @@
 package example.cashcard;
 
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +8,8 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
@@ -18,10 +18,24 @@ class CashCardApplicationTests {
     TestRestTemplate restTemplate;
 
     @Test
+    void shouldNotReturnACashCardWithAnUnknownId() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isBlank();
+    }
+
+    @Test
     void shouldReturnACashCardWhenDataIsSaved() {
         ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isNotNull();
+        Double amount = documentContext.read("$.amount");
+        assertThat(amount).isEqualTo(123.45);
     }
 
     @Test
